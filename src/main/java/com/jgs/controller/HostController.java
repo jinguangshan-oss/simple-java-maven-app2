c2420109c74777699a49a43bab530f7db62f19f5
jinguangshan <qiaozhi31002@163.com>
2021-02-07 10:37:29 +0800

    20210207

--- src/main/java/com/jgs/controller/HostController.java
+++ src/main/java/com/jgs/controller/HostController.java
@@ -1,20 +1,16 @@
 package com.jgs.controller;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @RestController
 public class HostController {
 
     @RequestMapping(path = "/host", method = RequestMethod.GET)
-    String home() throws UnknownHostException {
-        InetAddress ip4 = Inet4Address.getLocalHost();
-        return ip4.getHostAddress();
+    String home() {
+
+        return "http://47.103.199.109:8812/";
     }
 }
